f65faad96bee0af337d2de60d4ccf75446962cd2
Kunal Pal <dev.palkunal2@gmail.com>
2026-04-22 11:10:10 +0530

    fix api url for production

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
  //baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: "https://loansphere-production.up.railway.app/api",
   timeout: 15000,
 });
 
